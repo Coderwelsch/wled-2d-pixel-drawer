63268db2d76386799b39b380bb0d80232fc3e57f
Joseph Ribbe <social@coderwelsch.com>
2025-02-01 16:17:56 +0100

    fixed HomeView.spec.ts

---
+++ src/components/__tests__/HomeView.spec.ts
@@ -0,0 +1,11 @@
+import HomeView from '@/views/HomeView.vue'
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+
+describe('HomeView', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HomeView)
+    expect(wrapper.text()).toContain('PIXEL DI/SPLAY')
+  })
+})
