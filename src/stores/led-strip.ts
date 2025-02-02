df4d15ea780ac9888cfe11d2dc3ddef348a0b303
Joseph Ribbe <social@coderwelsch.com>
2025-02-02 23:46:14 +0100

    reworked storage management

--- src/stores/led-strip.ts
+++ src/stores/led-strip.ts
@@ -1,5 +1,9 @@
-import { onMounted, ref, watch } from 'vue'
+import { generateSerpentineData } from '@/lib/generate-serpentine-data.ts'
+import { getLocalStorage, setLocalStorage } from '@/lib/local-storage.ts'
+import { useWebSocket } from '@/lib/web-socket.ts'
+import { onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import { defineStore } from 'pinia'
+import { useToast } from 'vue-toast-notification'
 
 export interface GridPixelData {
   [y: number]: {
@@ -12,23 +16,33 @@ interface LedStripStore {
   cols: number
   rows: number
   brightness: number
-  jsonApiUrl: string
+  websocketUrl: string
   drawingColor: string
 }
 
-export const useLedStripStore = defineStore('led-strip', () => {
-  const baseJsonApiUrl = `http://${window.location.hostname}:${window.location.port || '80'}/json`
+const SETTINGS_STORAGE_KEY = 'led-strip-settings'
+const PIXEL_DATA_STORAGE_KEY = 'led-strip-pixel-data'
 
-  const settings = ref<LedStripStore>({
+export const useLedStripStore = defineStore('led-strip', () => {
+  const baseWebSocketUrl = `http://${window.location.hostname}:${window.location.port || '80'}`
+
+  const localStorageSettings = getLocalStorage<LedStripStore>(SETTINGS_STORAGE_KEY, {
     brightness: 30,
-    jsonApiUrl: baseJsonApiUrl,
+    websocketUrl: baseWebSocketUrl,
     drawingColor: '#FF0000',
     scale: 32,
     cols: 7,
     rows: 12,
   })
 
-  const pixelData = ref<GridPixelData>({})
+  const localStoragePixelData = getLocalStorage<GridPixelData>(PIXEL_DATA_STORAGE_KEY, {})
+  const settings = ref<LedStripStore>(localStorageSettings)
+  const pixelData = ref<GridPixelData>(localStoragePixelData)
+
+  const { ws: websocket } = useWebSocket(settings.value.websocketUrl, true)
+  const startTime = ref(performance.now())
+
+  const toast = useToast()
 
   const setPixel = (x: number, y: number) => {
     if (!pixelData.value[y]) {
@@ -37,25 +51,8 @@ export const useLedStripStore = defineStore('led-strip', () => {
 
     pixelData.value[y][x] = settings.value.drawingColor
 
+    setLocalStorage(PIXEL_DATA_STORAGE_KEY, pixelData.value)
     triggerSync()
-  }
-
-  const generateSerpentineData = (colors: string[]) => {
-    const data = []
-
-    for (let y = 0; y < settings.value.rows; y++) {
-      const row = []
-
-      for (let x = 0; x < settings.value.cols; x++) {
-        const color =
-          colors[y * settings.value.cols + (y % 2 === 0 ? x : settings.value.cols - x - 1)]
-        row.push(color ? color : '#000000')
-      }
-
-      data.push(row)
-    }
-
-    return data.flat()
   }
 
   const reset = () => {
@@ -78,12 +75,21 @@ export const useLedStripStore = defineStore('led-strip', () => {
   const isLoading = ref(false)
 
   const sendData = async () => {
+    if (!websocket.value || websocket.value.readyState !== WebSocket.OPEN) {
+      console.error('WebSocket not initialized')
+      return
+    }
+
+    const flatData = (
+      Object.values(pixelData.value)
+        .map((row) => Object.values(row))
+        .flat() as string[]
+    ).map((color: string) => color.replace('#', ''))
+
     const transformedData = generateSerpentineData(
-      (
-        Object.values(pixelData.value)
-          .map((row) => Object.values(row))
-          .flat() as string[]
-      ).map((color: string) => color.replace('#', '')),
+      flatData,
+      settings.value.rows,
+      settings.value.cols,
     )
 
     const payload = {
@@ -91,21 +97,18 @@ export const useLedStripStore = defineStore('led-strip', () => {
       bri: settings.value.brightness,
       len: settings.value.cols * settings.value.rows,
       seg: {
-        i: transformedData,
+        // TODO: remove
+        i: transformedData.slice(0, 40),
       },
     }
 
     isLoading.value = true
+    startTime.value = performance.now()
+    websocket.value.send(JSON.stringify(payload))
 
-    await fetch(settings.value.jsonApiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    }).finally(() => {
-      isLoading.value = false
-    })
+    // calculate size of data
+    const size = new Blob([JSON.stringify(payload)]).size
+    console.log(`Data sent to server: ${size} bytes`)
   }
 
   const triggerSync = () => {
@@ -113,20 +116,71 @@ export const useLedStripStore = defineStore('led-strip', () => {
       clearTimeout(autoUpdaterTimeout.value)
     }
 
-    autoUpdaterTimeout.value = setTimeout(() => {
+    if (isLoading.value) {
+      autoUpdaterTimeout.value = setTimeout(() => {
+        sendData()
+      }, 50) as unknown as number
+    } else {
       sendData()
-    }, 150) as unknown as number
+    }
+  }
+
+  const initWebSocket = () => {
+    const hostname = settings.value.websocketUrl.replace('http://', '').replace('https://', '')
+
+    websocket.value = new WebSocket(`ws://${hostname}/ws`)
+
+    websocket.value.onopen = () => {
+      console.log('WebSocket Opened')
+    }
+
+    websocket.value.onmessage = (event) => {
+      isLoading.value = false
+
+      const data = JSON.parse(event.data)
+
+      if (data.error) {
+        toast.error(`Error: ${data.error}`, {
+          position: 'top',
+        })
+      }
+
+      console.log('Time taken to send data:', performance.now() - startTime.value)
+    }
+
+    websocket.value.onerror = (error) => {
+      isLoading.value = false
+
+      console.error('WebSocket Error:', error)
+      toast.error('WebSocket connection error. Reconnecting …', {
+        position: 'top',
+      })
+    }
+
+    websocket.value.onclose = () => {
+      isLoading.value = false
+
+      console.log('WebSocket Closed')
+
+      toast.error('WebSocket connection closed. Reconnecting …', {
+        position: 'top',
+      })
+    }
   }
 
   onMounted(() => {
     reset()
+    initWebSocket()
+  })
+
+  onBeforeUnmount(() => {
+    if (websocket.value) {
+      websocket.value.close()
+    }
   })
 
   watch(settings.value, () => {
-    triggerSync()
-  })
-
-  watch(pixelData.value, () => {
+    setLocalStorage(SETTINGS_STORAGE_KEY, settings.value)
     triggerSync()
   })
 
