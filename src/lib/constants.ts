f5db8316e70849266cfbf985f56e05d7aedc07bd
Joseph Ribbe <social@coderwelsch.com>
2025-02-05 00:12:19 +0100

    added env variables

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,14 @@
 export const IS_DEV = import.meta.env.DEV || false
 
+export const DISABLE_BRIGHTNESS_CHANGE = import.meta.env.VITE_DISABLE_BRIGHTNESS_CHANGE || false
+export const DEFAULT_BRIGHTNESS = import.meta.env.VITE_DEFAUT_BRIGHTNESS || 30
+
+export const DISABLE_HOSTNAME_CHANGE = import.meta.env.VITE_DISABLE_HOSTNAME_CHANGE || false
+export const DEFAULT_HOSTNAME = import.meta.env.VITE_DEFAULT_HOSTNAME || window.location.hostname
+
+export const DEFAULT_COLS = import.meta.env.VITE_MATRIX_WIDTH || 30
+export const DEFAULT_ROWS = import.meta.env.VITE_MATRIX_HEIGHT || 30
+
 export const COLOR_PRESETS = [
 	{
 		color: "#000000",
