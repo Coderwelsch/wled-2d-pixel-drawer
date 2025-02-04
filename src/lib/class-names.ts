12a331bd0390dbf7b79b20cf5027487a8cac8b98
Joseph Ribbe <social@coderwelsch.com>
2025-02-05 00:13:15 +0100

    added tw-merge

--- src/lib/class-names.ts
+++ src/lib/class-names.ts
@@ -1,3 +1,5 @@
-export const classNames = (...classes: never[]): string => {
-	return classes.filter(Boolean).join(" ")
+import { twMerge } from "tailwind-merge"
+
+export const classNames = (...classes: unknown[]): string => {
+	return twMerge(classes.filter(Boolean).join(" "))
 }
