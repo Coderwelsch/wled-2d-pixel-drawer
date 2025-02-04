12ab0583cc42429799a50305a581acef54a393d8
Joseph Ribbe <social@coderwelsch.com>
2025-02-04 23:16:59 +0100

    added MobileActionsBar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import MobileActionsBar from "@/components/MobileActionsBar.vue"
 import SidebarItem from "@/components/SidebarItem.vue"
 import WledCanvasDrawer from "@/components/WledCanvasDrawer.vue"
 </script>
 
 <template>
-	<div class="flex h-screen w-screen flex-col-reverse gap-6 overflow-hidden lg:flex-row lg:gap-0">
-		<div class="w-full items-center justify-center">
+	<div class="flex w-full flex-col-reverse gap-6 overflow-hidden pb-16 md:h-screen md:pb-0 lg:flex-row lg:gap-0">
+		<div class="w-full p-6 md:items-center md:justify-center">
 			<WledCanvasDrawer />
 		</div>
 
 		<SidebarItem />
+
+		<MobileActionsBar />
 	</div>
 </template>
