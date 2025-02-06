652f6158551395ce964aa81793388355696c6233
Joseph Ribbe <social@coderwelsch.com>
2025-02-06 16:39:32 +0100

    added rgbToHex

--- src/lib/color-helpers.ts
+++ src/lib/color-helpers.ts
@@ -12,6 +12,10 @@ const hexToRgb = (hex: string) => {
 	}
 }
 
+export const rgbToHex = (r: number, g: number, b: number) => {
+	return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`
+}
+
 export const getHighContrastColor = (color: string) => {
 	const rgb = hexToRgb(color)
 
