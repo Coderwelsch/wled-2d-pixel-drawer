d106a5e107a5c5f2604958f4364ee80df62b43d6
Joseph Ribbe <social@coderwelsch.com>
2025-02-08 22:44:41 +0100

    added HorizontalDivider.vue

---
+++ src/components/HorizontalDivider.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { classNames } from "@/lib/class-names.ts"
+
+const props = defineProps<{
+	class?: string
+}>()
+</script>
+
+<template>
+	<hr :class="classNames('w-full border-neutral-700', props.class)" />
+</template>
+
+<style scoped></style>
