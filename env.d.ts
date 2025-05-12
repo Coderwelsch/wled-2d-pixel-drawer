8af74822314c977d78dd608d72846c3f9f941781
Joseph Ribbe <social@coderwelsch.com>
2025-05-12 22:36:12 +0200

    applied formatting

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-declare module  'snapsvg-cjs' {
-    const Snap: any;
-    export default Snap;
+declare module "snapsvg-cjs" {
+	const Snap: any
+	export default Snap
 }
