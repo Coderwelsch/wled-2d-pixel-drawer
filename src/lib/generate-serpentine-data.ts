082441d953672d8e0931529d1f3352d8d4dfe5f6
Joseph Ribbe <social@coderwelsch.com>
2025-05-19 10:04:56 +0200

    renamed colors to pixelData

--- src/lib/generate-serpentine-data.ts
+++ src/lib/generate-serpentine-data.ts
@@ -1,11 +1,11 @@
-export const generateSerpentineData = (colors: string[], rows: number, cols: number): string[] => {
+export const generateSerpentineData = (pixelData: string[], rows: number, cols: number): string[] => {
 	const data: string[][] = []
 
 	for (let y = 0; y < rows; y++) {
 		const row = []
 
 		for (let x = 0; x < cols; x++) {
-			const color = colors[y * cols + (y % 2 === 0 ? x : cols - x - 1)]
+			const color = pixelData[y * cols + (y % 2 === 0 ? x : cols - x - 1)]
 			row.push(color ? color : "#000000")
 		}
 
